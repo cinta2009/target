98c6a977d67b20fa82c212f4385b84063aa4be31
cinta2009 <21cintatata@gmail.com>
2026-01-13 14:36:14 +0700

    update

---
+++ update.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Update Data</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <?php
+    include 'config.php';
+
+    $id = $_GET['id'];
+
+    // ambil data lama
+    $sql = mysqli_query($koneksi, "SELECT * FROM tbjadwal WHERE id='$id'");
+    $siswa = mysqli_fetch_assoc($sql);
+
+    // proses update
+    if (isset($_POST['update'])) {
+        $bulan = $_POST['bulan'];
+        $target = $_POST['target'];
+        $todo = $_POST['to do'];
+
+        mysqli_query($koneksi, "UPDATE tbjadwal 
+        SET bulan='$bulan', target='$target',to do='$todo' 
+        WHERE id='$id'");
+
+        header("Location: view-data.php");
+        exit;
+    }
+    ?>
+
+    <div class="container">
+        <h2>Update Data Siswa</h2>
+
+        <form method="post">
+            <label>bulan</label>
+            <input type="text" name="bulan" value="<?= htmlspecialchars($siswa['bulan']); ?>" required>
+
+            <label>target</label>
+            <input type="text" name="target" value="<?= htmlspecialchars($siswa['target']); ?>" required>
+
+            <label>to do</label>
+            <input type="text" name="to do" value="<?= htmlspecialchars($siswa['to do']); ?>" required>
+
+            <input type="submit" name="update" value="Update">
+        </form>
+
+        <br>
+        <a href="view-data.php"><button>Kembali</button></a>
+    </div>
+
+</body>
+
+</html>
