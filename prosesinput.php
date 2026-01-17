75ce6bad3fb806499d2adc1d4ae719bac79b78f2
cinta2009 <21cintatata@gmail.com>
2026-01-17 18:28:28 +0700

    target

--- prosesinput.php
+++ prosesinput.php
@@ -6,7 +6,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $bulan = $_POST['bulan'];
     $target = $_POST['target'];
     $to_do = $_POST['to_do'];
-
+    
     // insert data ke database
     $sql = "INSERT INTO tbjadwal (bulan, target, to_do) VALUES ('$bulan', '$target','$to_do')";
     $result = mysqli_query($koneksi, $sql);
