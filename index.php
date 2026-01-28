c84d859b7edff1881c8678206a718b2b7a0fb1dd
cinta2009 <21cintatata@gmail.com>
2026-01-28 09:30:12 +0700

    perbaikan codingan

--- index.php
+++ index.php
@@ -5,6 +5,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>jadwalbulanan</title>
-    <style>
-    </head><body><?php include 'config.php'
-    ?><form action="prosesinput.php"method="post"><label>bulan</label><input type="text"name="bulan"><label>target</label><input type="text"name="target"><label>to_do</label><input type="text"name="to_do"><input type="submit"value="kirim"></form></body></html>
+</head>
+
+<body>
+    <?php
+    include 'config.php'
+    ?>
+
+    <form action="prosesinput.php" method="post">
+        <label>bulan</label>
+        <input type="text" name="bulan">
+        <label>target</label>
+        <input type="text" name="target">
+        <label>to_do</label>
+        <input type="text" name="to_do">
+        <input type="submit" value="kirim">
+    </form>
+</body>
+
+</html>
